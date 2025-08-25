76763bb84a76479f02944f219a46f1f43855327a
Dheemanth <dheemanth.vadde@sagarsoft.in>
2025-08-25 18:19:24 +0530

    db pool connection

--- config/db.js
+++ config/db.js
@@ -1,36 +1,15 @@
 const { Pool } = require("pg");
 
-const globalForPg = global;
+const pool = new Pool({
+  user: "sentrifugo",
+  host: "101.53.133.152",          
+  database: "silbob",
+  password: "pI01zRjCzyaxGqt",
+  port: 5432,                     
+});
 
-let pool = globalForPg.pgPool;
-if (!pool) {
-  const pool = new Pool({
-    user: "sentrifugo",
-    host: "101.53.133.152",
-    database: "silbob",
-    password: "pI01zRjCzyaxGqt",
-    port: 5432,
-    // important: cap connections from this app
-    max: 10, // adjust to your DB capacity
-    idleTimeoutMillis: 30000, // free idle clients after 30s
-    connectionTimeoutMillis: 5000,
-  });
-
-  pool
-    .query("SELECT 1")
-    .then(() => {
-      console.log("✅ PostgreSQL reachable");
-    })
-    .catch((err) => {
-      console.error("❌ PostgreSQL health check failed:", err.message);
-    });
-
-  pool.on("error", (err) => {
-    console.error("Unexpected PG pool error:", err);
-  });
-  if (process.env.NODE_ENV !== "production") {
-    globalForPg.pgPool = pool;
-  }
-}
+pool.connect()
+  .then(() => console.log("✅ Connected to PostgreSQL"))
+  .catch(err => console.error("❌ Connection error", err.stack));
 
 module.exports = pool;
